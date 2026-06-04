29a01ceaace234883ce87e3f29f5db97b1ef6940
DevBee <walexsson@gmail.com>
2026-06-04 23:09:46 +0100

    add signup view to the admin panel

--- checkam/admin.py
+++ checkam/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Budget, Transactions
+from .models import Budget, Signup, Transactions
 
 # Register your models here.
 
+admin.site.register(Signup)
 admin.site.register(Transactions)
 admin.site.register(Budget)
