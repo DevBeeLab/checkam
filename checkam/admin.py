99f8b2848ac64e627a0e3ffc74f2aa6bee9c344a
DevBee <walexsson@gmail.com>
2026-06-04 22:38:06 +0100

    temporarily set DEBUG to True

--- checkam/admin.py
+++ checkam/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Transactions
+from .models import Budget, Transactions
 
 # Register your models here.
 
 admin.site.register(Transactions)
+admin.site.register(Budget)
